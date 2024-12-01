3009bb6ec26aef552fb979049a0c5f5d4d101d28
Tom Fielder <me@tomfielder.co.uk>
2024-12-01 10:18:08 +0000

    Add D1P2 solution,complete day 1

--- rust/src/bin/day1.rs
+++ rust/src/bin/day1.rs
@@ -2,9 +2,10 @@ const INPUT: &str = include_str!("../../../inputs/day1.txt");
 
 fn main() {
     println!("The result for day 1 part 1 is {}", part1(INPUT));
+    println!("The result for day 1 part 2 is {}", part2(INPUT));
 }
 
-fn part1(input: &str) -> i32 {
+fn read_input_to_vectors(input: &str) -> (Vec<i32>, Vec<i32>) {
     let mut first_vec: Vec<i32> = Vec::new();
     let mut second_vec: Vec<i32> = Vec::new();
 
@@ -20,6 +21,12 @@ fn part1(input: &str) -> i32 {
         }
     }
 
+    return (first_vec, second_vec);
+}
+
+fn part1(input: &str) -> i32 {
+    let (mut first_vec, mut second_vec) = read_input_to_vectors(input);
+
     // Sort vectors in descending order
     first_vec.sort_by(|a, b| b.cmp(a));
     second_vec.sort_by(|a, b| b.cmp(a));
@@ -32,6 +39,23 @@ fn part1(input: &str) -> i32 {
         .sum()
 }
 
+fn part2(input: &str) -> i32 {
+    let (first_vec, second_vec) = read_input_to_vectors(input);
+
+    // Don't need to sort the vectors this time
+    // (or modify them at all, so we can avoid the 'mut' keyword)
+    // for each item in first_vec, count how many times it appears in second_vec
+    // multiply that item by the count, and add the result to the total
+
+    let mut result: i32 = 0;
+
+    for item in first_vec {
+        let count = second_vec.iter().filter(|&&value| value == item).count() as i32;
+        result += item * count;
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     const TEST_INPUT: &str = include_str!("../../../testInputs/day1.txt");
@@ -40,5 +64,10 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(part1(TEST_INPUT), 11);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT), 31);
     }
 }
