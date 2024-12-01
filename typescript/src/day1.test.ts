e7a526520b283bba981a071b6284939e7bf537e8
Tom Fielder <me@tomfielder.co.uk>
2024-12-01 14:03:58 +0000

    Add ts solution for D1P1

--- typescript/src/day1.test.ts
+++ typescript/src/day1.test.ts
@@ -1,5 +1,15 @@
 import { expect, test } from "vitest"
+import { readFileSync } from "fs"
+import { parseInputToArrays, part1 } from "./day1"
 
-test("Day 1 solution test", () => {
-    expect(1).toEqual(1)
+test("Day 1 Part 1", () => {
+    const input = readFileSync("../testInputs/day1.txt", "utf-8")
+
+    const [first, second] = parseInputToArrays(input)
+
+    const result = part1(first, second)
+
+    expect(result).toEqual(11)
 })
+
+test("Day 1 Part 2", () => {})
--- typescript/src/day1.ts
+++ typescript/src/day1.ts
@@ -1,1 +1,38 @@
-console.log("This is day 1, saying hello")
+import { readFileSync } from "fs"
+
+const input = readFileSync("../inputs/day1.txt", "utf-8")
+
+export const parseInputToArrays = (input: string): number[][] => {
+    const lines = input.split("\r\n")
+
+    const firstArr: number[] = []
+    const secondArr: number[] = []
+
+    lines.forEach((line) => {
+        // Split the line at whitespace to get the two values
+        const [first, second] = line.split(/\s+/)
+        // Push them to the output arrays
+        firstArr.push(Number(first))
+        secondArr.push(Number(second))
+    })
+
+    return [firstArr, secondArr]
+}
+
+export const part1 = (first: number[], second: number[]): number => {
+    // Sort both arrays
+    first.sort((a, b) => a - b)
+    second.sort((a, b) => a - b)
+
+    const output = first.reduce((acc, val, index) => {
+        return (acc += Math.abs(val - second[index]))
+    }, 0)
+
+    return output
+}
+
+const [first, second] = parseInputToArrays(input)
+
+const part1Result = part1(first, second)
+
+console.log("The result for day 1 part 1 is ", part1Result)
