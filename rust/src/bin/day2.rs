25c941b5044db095b9bf89116955969a642538b4
Tom Fielder <me@tomfielder.co.uk>
2024-12-02 07:26:27 +0000

    Add D2P2 rust solution

--- rust/src/bin/day2.rs
+++ rust/src/bin/day2.rs
@@ -2,6 +2,7 @@ const INPUT: &str = include_str!("../../../inputs/day2.txt");
 
 fn main() {
     println!("The result for day 2 part 1 is {}", part1(INPUT));
+    println!("The result for day 2 part 2 is {}", part2(INPUT));
 }
 
 fn part1(input: &str) -> i32 {
@@ -37,6 +38,51 @@ fn part1(input: &str) -> i32 {
     safe
 }
 
+// Filthy - copy and paste the whole thing from part 1 into a disgusting one-liner. Let's not talk about this
+fn is_safe(levels: &[i32]) -> bool {
+    levels
+        .iter()
+        .zip(levels.iter().skip(1))
+        .all(|(a, b)| a > b && (a - b).abs() >= 1 && (a - b).abs() <= 3)
+        || levels
+            .iter()
+            .zip(levels.iter().skip(1))
+            .all(|(a, b)| a < b && (a - b).abs() >= 1 && (a - b).abs() <= 3)
+}
+
+fn part2(input: &str) -> i32 {
+    let reports = input.lines();
+
+    let mut safe: i32 = 0;
+
+    for level in reports {
+        let levels: Vec<i32> = level
+            .split_whitespace()
+            .map(|item| item.parse::<i32>().unwrap())
+            .collect();
+
+        // Check if it's already safe, mark and and move on if so (ie don't check the next bit)
+        if is_safe(&levels) {
+            safe += 1;
+            continue;
+        }
+
+        // Check if removing one element makes it safe
+        for i in 0..levels.len() {
+            let mut temp = levels.clone();
+            temp.remove(i);
+            // is it safe with the ith element removed?
+            if is_safe(&temp) {
+                safe += 1;
+                // No need to keep looking, we only need to prove it once
+                break;
+            }
+        }
+    }
+
+    safe
+}
+
 #[cfg(test)]
 mod tests {
     const TEST_INPUT: &str = include_str!("../../../testInputs/day2.txt");
@@ -45,5 +91,10 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(part1(TEST_INPUT), 2);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT), 4);
     }
 }
