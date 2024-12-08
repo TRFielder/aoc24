d44d70cf32732e4ffa651dd868e3f06d48a369cd
Tom Fielder <me@tomfielder.co.uk>
2024-12-08 12:35:55 +0000

    Add D8P2 rust solution
    Part 1: 541.5µs
    Part 2: 1.4279ms

--- rust/src/bin/day8.rs
+++ rust/src/bin/day8.rs
@@ -11,16 +11,25 @@ pub fn main() {
     let map = read_input_to_map(INPUT);
 
     let part1_start = Instant::now();
-    let part1_result = part1(&map);
+    let part1_result = solve(&map, false);
     let part1_duration = part1_start.elapsed();
 
     println!(
         "The result for day 8 part 1 is {}, it took {:?}",
         part1_result, part1_duration
     );
+
+    let part2_start = Instant::now();
+    let part2_result = solve(&map, true);
+    let part2_duration = part2_start.elapsed();
+
+    println!(
+        "The result for day 8 part 2 is {}, it took {:?}",
+        part2_result, part2_duration
+    );
 }
 
-fn part1(input: &HashMap<(usize, usize), char>) -> usize {
+fn solve(input: &HashMap<(usize, usize), char>, with_multiple: bool) -> usize {
     // We need to find all the unique antennas (characters) in the map
     let antenna_types: HashSet<char> = input
         .values()
@@ -66,23 +75,57 @@ fn part1(input: &HashMap<(usize, usize), char>) -> usize {
 
                     // Get positions in both directions from both antennas
                     for direction in [-1, 1] {
-                        let antinode_x1 = x1 as isize + (direction * dx);
-                        let antinode_y1 = y1 as isize + (direction * dy);
+                        // We can go multiples of the vector so long as at least one is within the bounds of the map
+                        let mut multiple = 1;
 
-                        let antinode_x2 = x2 as isize + (direction * dx);
-                        let antinode_y2 = y2 as isize + (direction * dy);
+                        loop {
+                            let antinode_x1 = x1 as isize + (direction * multiple * dx);
+                            let antinode_y1 = y1 as isize + (direction * multiple * dy);
 
-                        let pos1 = (antinode_x1 as usize, antinode_y1 as usize);
-                        let pos2 = (antinode_x2 as usize, antinode_y2 as usize);
+                            let antinode_x2 = x2 as isize + (direction * multiple * dx);
+                            let antinode_y2 = y2 as isize + (direction * multiple * dy);
 
-                        // If the antinodes generated don't overlap the antenna pair, put it in the Set
-                        // (I expect one will overlap each time but that's ok)
-                        if pos1.0 < cols && pos1.1 < rows && !positions.contains(&pos1) {
-                            antinode_positions.insert(pos1);
-                        }
+                            let pos1 = (antinode_x1 as usize, antinode_y1 as usize);
+                            let pos2 = (antinode_x2 as usize, antinode_y2 as usize);
 
-                        if pos2.0 < cols && pos2.1 < rows && !positions.contains(&pos2) {
-                            antinode_positions.insert(pos2);
+                            // Break the loop if no antinode is within the map
+                            if (antinode_x1 < 0
+                                || antinode_y1 < 0
+                                || antinode_x1 >= cols as isize
+                                || antinode_y1 >= rows as isize)
+                                && (antinode_x2 < 0
+                                    || antinode_y2 < 0
+                                    || antinode_x2 >= cols as isize
+                                    || antinode_y2 >= rows as isize)
+                            {
+                                break;
+                            }
+
+                            // If the antinodes generated don't overlap the antenna pair, put it in the Set
+                            // (I expect one will overlap each time but that's ok)
+                            // If we're allowing resonance (part 2) allow overlap
+                            if pos1.0 < cols && pos1.1 < rows {
+                                if with_multiple {
+                                    antinode_positions.insert(pos1);
+                                } else if !with_multiple && !positions.contains(&pos1) {
+                                    antinode_positions.insert(pos1);
+                                }
+                            }
+
+                            if pos2.0 < cols && pos2.1 < rows {
+                                if with_multiple {
+                                    antinode_positions.insert(pos2);
+                                } else if !with_multiple && !positions.contains(&pos2) {
+                                    antinode_positions.insert(pos2);
+                                }
+                            }
+
+                            // Add loop breaker so part 1 still works
+                            if !with_multiple {
+                                break;
+                            }
+                            // Increment the multiplier for the next loop
+                            multiple += 1;
                         }
                     }
                 }
@@ -102,6 +145,12 @@ mod test {
     #[test]
     fn test_part1() {
         let map = read_input_to_map(TEST_INPUT);
-        assert_eq!(part1(&map), 14);
+        assert_eq!(solve(&map, false), 14);
+    }
+
+    #[test]
+    fn test_part2() {
+        let map = read_input_to_map(TEST_INPUT);
+        assert_eq!(solve(&map, true), 34);
     }
 }
