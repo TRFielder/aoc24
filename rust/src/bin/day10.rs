d5284f59372ab9f54c6167367a71d07b1411d99a
Tom Fielder <tom.fielder97@gmail.com>
2024-12-10 19:32:42 +0000

    Add D10P2 Rust solution
    Part 1: 9.5209ms
    Part 2: 9.1069ms

--- rust/src/bin/day10.rs
+++ rust/src/bin/day10.rs
@@ -14,6 +14,15 @@ pub fn main() {
         "The result for day 10 part 1 is {}, it took {:?}",
         part1_result, part1_duration
     );
+
+    let part2_start = Instant::now();
+    let part2_result = part2(INPUT);
+    let part2_duration = part2_start.elapsed();
+
+    println!(
+        "The result for day 10 part 2 is {}, it took {:?}",
+        part2_result, part2_duration
+    );
 }
 
 const DIRECTIONS: &[(isize, isize); 4] = &[
@@ -62,10 +71,47 @@ fn find_paths(
 
         if next_x >= 0 && next_y >= 0 {
             let next_pos = (next_x as usize, next_y as usize);
+            // If the position we're trying is a valid one
             if let Some(&next_value) = map.get(&next_pos) {
+                // And so is our current position (it SHOULD be)
                 if let Some(&current_value) = map.get(&(x, y)) {
                     if next_value == current_value + 1 {
+                        // Then keep going with the pathfinding recursion
                         find_paths(next_pos.0, next_pos.1, map, visited_peaks);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn find_unique_trails(
+    x: usize,
+    y: usize,
+    map: &HashMap<(usize, usize), u32>,
+    unique_trails: &mut usize,
+) {
+    // If we've found the peak, increment the trail counter and exit recursion
+    if let Some(&value) = map.get(&(x, y)) {
+        if value == 9 {
+            *unique_trails += 1;
+            return;
+        }
+    }
+
+    for &(dx, dy) in DIRECTIONS {
+        let next_x = x as isize + dx;
+        let next_y = y as isize + dy;
+
+        if next_x >= 0 && next_y >= 0 {
+            let next_pos = (next_x as usize, next_y as usize);
+            // If the position we're trying is a valid one
+            if let Some(&next_value) = map.get(&next_pos) {
+                // And so is our current position (it SHOULD be)
+                if let Some(&current_value) = map.get(&(x, y)) {
+                    if next_value == current_value + 1 {
+                        // Then keep going with the pathfinding recursion
+                        find_unique_trails(next_pos.0, next_pos.1, map, unique_trails);
                     }
                 }
             }
@@ -94,6 +140,28 @@ fn part1(input: &str) -> usize {
     result
 }
 
+fn part2(input: &str) -> usize {
+    let map = read_input(input);
+
+    let mut result: usize = 0;
+    // Iterate through every location in the map
+    for (&(x, y), &value) in &map {
+        // Only continue if we've found a trailhead
+        if value != 0 {
+            continue;
+        }
+
+        // Track how many paths we can follow to any peak from this spot
+        // (Just a counter, not a HashSet)
+        let mut trails: usize = 0;
+        // Do the pathfinding
+        find_unique_trails(x, y, &map, &mut trails);
+
+        result += trails
+    }
+    result
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -103,5 +171,10 @@ mod test {
     #[test]
     fn test_part1() {
         assert_eq!(part1(TEST_INPUT), 36);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT), 81);
     }
 }
