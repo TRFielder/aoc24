dd9497d9af1f4a22da5ec19fd254a2007ee752ba
Tom Fielder <tom.fielder97@gmail.com>
2024-12-11 13:39:45 +0000

    Add D11P2 Rust solution
    Part 1: 3.4554ms
    Part 2: 110.782ms
    
    Order doesn't actually matter, just the value of the stones themselves. I feel tricked.

--- rust/src/bin/day11.rs
+++ rust/src/bin/day11.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use std::{collections::HashMap, time::Instant};
 
 const INPUT: &str = include_str!("../../../inputs/day11.txt");
 
@@ -8,32 +8,61 @@ pub fn main() {
     let part1_duration = part1_start.elapsed();
 
     println!(
-        "The result for day 10 part 1 is {}, it took {:?}",
+        "The result for day 11 part 1 is {}, it took {:?}",
         part1_result, part1_duration
+    );
+
+    let part2_start = Instant::now();
+    let part2_result = part2(INPUT);
+    let part2_duration = part2_start.elapsed();
+
+    println!(
+        "The result for day 11 part 2 is {}, it took {:?}",
+        part2_result, part2_duration
     );
 }
 
 fn part1(input: &str) -> i64 {
-    let mut array: Vec<i64> = input
-        .split_whitespace()
-        .map(|val| return val.parse::<i64>().unwrap())
-        .collect();
+    // We can use a hashmap because every stone has the same outcome from the rules!
+    let mut stones: HashMap<i64, i64> = HashMap::new();
+
+    for stone in input.split_whitespace() {
+        let stone_num: i64 = stone.parse().unwrap();
+        // So we count how many of each stone there are
+        *stones.entry(stone_num).or_default() += 1;
+    }
 
     for _ in 0..25 {
-        array = blink(array);
+        stones = blink(stones);
     }
-    array.len() as i64
+    stones.values().sum()
 }
 
-fn blink(stones: Vec<i64>) -> Vec<i64> {
-    // Create a new vector to hold our modified stones
-    let mut new_stones: Vec<i64> = Vec::new();
+fn part2(input: &str) -> i64 {
+    // We can use a hashmap because every stone has the same outcome from the rules!
+    let mut stones: HashMap<i64, i64> = HashMap::new();
+
+    for stone in input.split_whitespace() {
+        let stone_num: i64 = stone.parse().unwrap();
+        // So we count how many of each stone there are
+        *stones.entry(stone_num).or_default() += 1;
+    }
+
+    for _ in 0..75 {
+        stones = blink(stones);
+    }
+    stones.values().sum()
+}
+
+fn blink(stones: HashMap<i64, i64>) -> HashMap<i64, i64> {
+    // Create a new hashmap to hold our modified stones
+    let mut new_stones: HashMap<i64, i64> = HashMap::new();
 
     // Iterate over the stones and apply the rules, in order.
-    for stone in stones.iter() {
+    for (stone, count) in stones {
         // Replace 0 with 1 in the new vector
-        if stone == &0i64 {
-            new_stones.push(1);
+        if stone == 0 {
+            *new_stones.entry(1).or_default() += count;
             // Skip further rules
             continue;
         }
@@ -46,16 +75,16 @@ fn blink(stones: Vec<i64>) -> Vec<i64> {
             let left: i64 = stone_str[..mid].parse().unwrap();
             let right: i64 = stone_str[mid..].parse().unwrap();
 
-            // Add the split numbers to the new vector
-            new_stones.push(left);
-            new_stones.push(right);
+            // Add the split numbers to the new hashmap
+            *new_stones.entry(left).or_default() += count;
+            *new_stones.entry(right).or_default() += count;
 
             // Skip further rules
             continue;
         }
 
         // If the other rules don't apply, multiply stone by 2024
-        new_stones.push(stone * 2024);
+        *new_stones.entry(stone * 2024).or_default() += count;
     }
 
     new_stones
@@ -71,4 +100,6 @@ mod test {
     fn test_part1() {
         assert_eq!(part1(TEST_INPUT), 55312);
     }
+
+    // No need to test for part 2. It's the same thing just longer...
 }
