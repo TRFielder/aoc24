7e188ebe095339488e8bdad9de23705ab0b2b509
Tom Fielder <tom.fielder97@gmail.com>
2024-12-14 19:08:57 +0000

    Add D14P2 rust/manual solution
    Part 1: 893.5µs
    Part 2: 7.2249509s
    (plus the time it took me to flick through 10000 images to find a christmas tree)
    
    Don't judge me.

--- rust/src/bin/day14.rs
+++ rust/src/bin/day14.rs
@@ -1,6 +1,6 @@
-use std::time::Instant;
-
+use image::{GrayImage, Luma};
 use regex::Regex;
+use std::{fs, path::Path, time::Instant};
 
 const INPUT: &str = include_str!("../../../inputs/day14.txt");
 
@@ -10,8 +10,17 @@ pub fn main() {
     let part1_duration = part1_start.elapsed();
 
     println!(
-        "The result for day 13 part 1 is {}, it took {:?}",
+        "The result for day 14 part 1 is {}, it took {:?}",
         part1_result, part1_duration
+    );
+
+    let part2_start = Instant::now();
+    part2(INPUT, 101, 103);
+    let part2_duration = part2_start.elapsed();
+
+    println!(
+        "The result for day 14 part 2 took {:?}. Good luck finding the right image!",
+        part2_duration
     );
 }
 
@@ -43,6 +52,33 @@ fn part1(input: &str, x_max: i32, y_max: i32) -> i32 {
     count_robots(robots, x_max, y_max)
 }
 
+// Copy part 1's code but with a twist...
+fn part2(input: &str, x_max: i32, y_max: i32) {
+    let exp = Regex::new(r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)").unwrap();
+
+    // Get a vector of tuples of all the info for a given robot. x,y position first, then x,y velocity
+    let mut robots: Vec<((i32, i32), (i32, i32))> = Vec::new();
+
+    for line in input.lines() {
+        if let Some(capture) = exp.captures(line) {
+            robots.push((
+                (
+                    capture[1].parse::<i32>().unwrap(),
+                    capture[2].parse::<i32>().unwrap(),
+                ),
+                (
+                    capture[3].parse::<i32>().unwrap(),
+                    capture[4].parse::<i32>().unwrap(),
+                ),
+            ));
+        }
+    }
+
+    // Simulate for a 10000 seconds of movement and save an image for each second.
+    // This is gonna be fun...
+    move_robots_and_save_image(robots, x_max, y_max, 10000);
+}
+
 // Returns the robots' positions as a vector after duration seconds of movement
 fn move_robots(
     robots: Vec<((i32, i32), (i32, i32))>,
@@ -61,6 +97,38 @@ fn move_robots(
             )
         })
         .collect()
+}
+
+// Returns the robots' positions as a vector after duration seconds of movement, and saves an image of the positions
+fn move_robots_and_save_image(
+    robots: Vec<((i32, i32), (i32, i32))>,
+    x_max: i32,
+    y_max: i32,
+    duration: i32,
+) {
+    let output_dir = "./day14img";
+    fs::create_dir_all(output_dir).unwrap();
+    for i in 1..duration {
+        let new_positions: Vec<(i32, i32)> = robots
+            .iter()
+            .map(|((x, y), (dx, dy))| {
+                (
+                    // position = velocity * time
+                    // Thanks rust for having handy remainder functions for this exact problem...
+                    (x + dx * i).rem_euclid(x_max),
+                    (y + dy * i).rem_euclid(y_max),
+                )
+            })
+            .collect();
+
+        // Print every robot's position to the image grid
+        let mut img = GrayImage::new(x_max as u32, y_max as u32);
+        for (x, y) in new_positions.iter() {
+            img.put_pixel(*x as u32, *y as u32, Luma([255]));
+        }
+        let file_path = format!("{}/{}.png", output_dir, i);
+        img.save(Path::new(&file_path)).unwrap();
+    }
 }
 
 fn count_robots(robots: Vec<(i32, i32)>, x_max: i32, y_max: i32) -> i32 {
